260e4a5f3e9135d34313558119561c446d8e4d71
supa-modo <eddieochieng11@gmail.com>
2025-02-23 23:38:33 +0300

    minor fixes

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -63,15 +63,15 @@ const AuthPage = () => {
                 <img src="/joblogo.jpg" alt="logo" />
               </motion.div>
               <motion.h1
-                className="text-3xl font-nunito font-extrabold tracking-tight mb-2 text-center"
+                className="text-2xl font-nunito font-extrabold tracking-tight mb-2 text-center"
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.4, duration: 0.4, ease: "easeOut" }}
               >
-                Company Name Example
+                Company Name Placeholder
               </motion.h1>
               <motion.h2
-                className="text-lg font-semibold uppercase mb-6 text-center"
+                className="text- font-semibold uppercase mb-10 text-center text-gray-300 underline underline-offset-8"
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.3, duration: 0.5, ease: "easeOut" }}
@@ -91,7 +91,9 @@ const AuthPage = () => {
                       <TbClipboardText className="w-6 h-6 text-white" />
                     </div>
                     <div>
-                      <p className="font-semibold">Track Application Status</p>
+                      <p className="font-semibold text-green-600">
+                        Track Application Status
+                      </p>
                       <p className="text-sm opacity-90">
                         Monitor the progress of your submitted applications
                       </p>
@@ -103,7 +105,9 @@ const AuthPage = () => {
                       <TbEdit className="w-6 h-6 text-white" />
                     </div>
                     <div>
-                      <p className="font-semibold">Update Your Applications</p>
+                      <p className="font-semibold text-green-600">
+                        Update Your Applications
+                      </p>
                       <p className="text-sm opacity-90">
                         Complete or modify applications for open positions
                       </p>
@@ -115,7 +119,9 @@ const AuthPage = () => {
                       <PiUploadDuotone className="w-6 h-6 text-white" />
                     </div>
                     <div>
-                      <p className="font-semibold">Manage Your Documents</p>
+                      <p className="font-semibold text-green-600">
+                        Manage Your Documents
+                      </p>
                       <p className="text-sm opacity-90">
                         Upload or update resumes, cover letters, and other files
                       </p>
@@ -126,7 +132,7 @@ const AuthPage = () => {
             </div>
 
             <motion.p
-              className="text-xs mt-6 text-center italic text-amber-500"
+              className="text-xs mt-6 text-center italic text-amber-400"
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.3, duration: 0.5, ease: "easeOut" }}
@@ -143,7 +149,7 @@ const AuthPage = () => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.4, duration: 0.4, ease: "easeOut" }}
             >
-              <h2 className="text-3xl font-extrabold text-center text-primary-light mb-2">
+              <h2 className="text-2xl font-extrabold text-center text-primary-light mb-2">
                 {isLogin
                   ? "Sign In to Continue Application"
                   : "Create Your Applicant Account"}
@@ -162,105 +168,133 @@ const AuthPage = () => {
             </motion.div>
 
             <motion.form
-              className="space-y-5"
+              className="space-y-7  "
               onSubmit={handleSubmit}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 0.2 }}
             >
-              <AnimatePresence mode="wait">
-                {!isLogin && (
-                  <motion.div
-                    key="name"
-                    initial={{ opacity: 0, height: 0 }}
-                    animate={{ opacity: 1, height: "auto" }}
-                    exit={{ opacity: 0, height: 0 }}
-                    transition={{ duration: 0.3 }}
+              <div className="">
+                <AnimatePresence mode="wait">
+                  {!isLogin && (
+                    <motion.div
+                      key="name"
+                      initial={{ opacity: 0, height: 0 }}
+                      animate={{ opacity: 1, height: "auto" }}
+                      exit={{ opacity: 0, height: 0 }}
+                      transition={{ duration: 0.3 }}
+                      className="mb-3"
+                    >
+                      <label
+                        htmlFor="name"
+                        className="block text-sm font-bold font-nunito text-gray-700 mb-1"
+                      >
+                        Full Name
+                      </label>
+                      <div className="relative rounded-md shadow-sm">
+                        <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
+                          <PiUserDuotone className="h-5 w-5 text-gray-500" />
+                        </div>
+                        <input
+                          id="name"
+                          name="name"
+                          type="text"
+                          required
+                          className="block w-full pl-12 pr-3 py-2.5 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-primary-extralight focus:border-primary-extralight sm:text-sm transition duration-150 ease-in-out"
+                          placeholder="John Doe"
+                          value={formState.name}
+                          onChange={handleInputChange}
+                        />
+                      </div>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+                <div className="">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-bold font-nunito text-gray-700 mb-1"
                   >
+                    Your Email address
+                  </label>
+                  <div className="relative rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <TbMail className="h-5 w-5 text-gray-500" />
+                    </div>
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full pl-12 pr-3 py-2.5 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-primary-extralight focus:border-primary-extralight sm:text-sm transition duration-150 ease-in-out"
+                      placeholder="you@example.com"
+                      value={formState.email}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+
+                  <div className="mt-3">
                     <label
-                      htmlFor="name"
+                      htmlFor="password"
                       className="block text-sm font-bold font-nunito text-gray-700 mb-1"
                     >
-                      Full Name
+                      Password
                     </label>
                     <div className="relative rounded-md shadow-sm">
-                      <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
-                        <PiUserDuotone className="h-5 w-5 text-gray-500" />
+                      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <PiPasswordDuotone className="h-5 w-5 text-gray-600" />
                       </div>
                       <input
-                        id="name"
-                        name="name"
-                        type="text"
+                        id="password"
+                        name="password"
+                        type={showPassword ? "text" : "password"}
+                        autoComplete="current-password"
                         required
                         className="block w-full pl-12 pr-3 py-2.5 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-primary-extralight focus:border-primary-extralight sm:text-sm transition duration-150 ease-in-out"
-                        placeholder="John Doe"
-                        value={formState.name}
+                        placeholder="••••••••"
+                        value={formState.password}
                         onChange={handleInputChange}
                       />
+                      <div className="absolute inset-y-0 right-0 pr-4 flex items-center">
+                        <button
+                          type="button"
+                          onClick={() => setShowPassword(!showPassword)}
+                          className="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150"
+                        >
+                          {showPassword ? (
+                            <FaEyeSlash className="h-5 w-5" />
+                          ) : (
+                            <FaEye className="h-5 w-5" />
+                          )}
+                        </button>
+                      </div>
                     </div>
-                  </motion.div>
-                )}
-              </AnimatePresence>
 
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-bold font-nunito text-gray-700 mb-1"
-                >
-                  Your Email address
-                </label>
-                <div className="relative rounded-md shadow-sm">
-                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <TbMail className="h-5 w-5 text-gray-500" />
-                  </div>
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full pl-12 pr-3 py-2.5 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-primary-extralight focus:border-primary-extralight sm:text-sm transition duration-150 ease-in-out"
-                    placeholder="you@example.com"
-                    value={formState.email}
-                    onChange={handleInputChange}
-                  />
-                </div>
-              </div>
+                    <div className="flex items-center justify-between mt-3">
+                      <div className="flex items-center">
+                        <input
+                          id="remember-me"
+                          name="remember-me"
+                          type="checkbox"
+                          className="h-4 w-4 text-primary-light focus:ring-primary-light border-gray-300 rounded"
+                        />
+                        <label
+                          htmlFor="remember-me"
+                          className="ml-2 text-sm text-gray-600 hover:text-gray-800 transition-colors cursor-pointer"
+                        >
+                          Remember me
+                        </label>
+                      </div>
 
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-bold font-nunito text-gray-700 mb-1"
-                >
-                  Password
-                </label>
-                <div className="relative rounded-md shadow-sm">
-                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <PiPasswordDuotone className="h-5 w-5 text-gray-600" />
-                  </div>
-                  <input
-                    id="password"
-                    name="password"
-                    type={showPassword ? "text" : "password"}
-                    autoComplete="current-password"
-                    required
-                    className="block w-full pl-12 pr-3 py-2.5 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-primary-extralight focus:border-primary-extralight sm:text-sm transition duration-150 ease-in-out"
-                    placeholder="••••••••"
-                    value={formState.password}
-                    onChange={handleInputChange}
-                  />
-                  <div className="absolute inset-y-0 right-0 pr-4 flex items-center">
-                    <button
-                      type="button"
-                      onClick={() => setShowPassword(!showPassword)}
-                      className="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150"
-                    >
-                      {showPassword ? (
-                        <FaEyeSlash className="h-5 w-5" />
-                      ) : (
-                        <FaEye className="h-5 w-5" />
-                      )}
-                    </button>
+                      <div className="text-sm">
+                        <a
+                          href="#"
+                          className="font-bold font-nunito tracking-tight text-primary-light hover:text-sidebar transition-colors"
+                        >
+                          Forgot your password?
+                        </a>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
